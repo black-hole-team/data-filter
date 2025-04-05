509bb67d4b3d274e00fc067d9f36d5190da41e74
plekhanov <plekhanov@bifit.com>
2025-04-05 15:12:02 +0400

    DEV-8 Перенести модуль фильтрации в отдельный репозиторий
    - mongodb и jpa теперь опциональные зависимости

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -16,12 +16,12 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.apache.logging.log4j:log4j-core:${property("log4jVersion")}")
     implementation("org.apache.commons:commons-text:${property("commonsTextVersion")}")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework:spring-beans")
     implementation("org.springframework:spring-webmvc")
+    implementation("org.springframework:spring-beans")
     implementation("jakarta.servlet:jakarta.servlet-api")
     implementation(project(":backend"))
+    compileOnly("org.springframework.boot:spring-boot-starter-data-mongodb")
+    compileOnly("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks {
